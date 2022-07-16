0f53deafa800f2516284c8866178522b4e0f76cc
VuNgoDinh <ngodinhvuluan@gmail.com>
2022-07-16 10:51:17 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,120 @@
+
+
+
+import json
+
+
+from flask import Flask, render_template, request
+
+from flask_mail import Mail,Message
+
+app = Flask(__name__)
+
+
+mail_username='mail.otakime@gmail.com'
+mail_password='smwacblnqgibazdd'
+
+app.debug=True
+
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = mail_username
+app.config['MAIL_PASSWORD'] = mail_password
+
+mail = Mail(app)
+
+
+def parseJson():
+    with open('dbManga.json',encoding="utf8") as f:
+        data = json.loads(f.read())
+    return data
+
+def getallMangaIndex(nameManga):
+    dictMangaIndex ={}
+    temp= 0
+    for key,value in getallManga():
+        if nameManga == value['nameManga']:
+            dictMangaIndex.update({key:value})
+            temp = temp +1
+            if temp == 4:
+                break
+    return dictMangaIndex.items()
+    
+
+def getallManga():
+    index_list = 0
+    dictMangaIndex ={}
+    for key , value in parseJson()[index_list].items():
+        dictMangaIndex.update({key:value})
+    return dictMangaIndex.items()
+
+
+
+@app.route("/")
+def home():
+    dictMangaIndex ={}
+    temp= 0
+    for key,value in getallManga():
+        dictMangaIndex.update({key:value})
+        temp = temp +1
+        if temp == 4:
+            break
+    
+    dictMangaIndex2 ={}
+    temp= 4
+    for key,value in getallManga():
+        dictMangaIndex.update({key:value})
+        temp = temp +1
+        if temp == 7:
+            break
+    list_dictMangaIndex2 = list(dictMangaIndex2)
+
+    return render_template('index.html', data = dictMangaIndex.items(), dataIndex = reversed(list_dictMangaIndex2)) 
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/contact", methods = ['GET','POST'])
+def contact():
+    if request.method == "POST":
+        name = request.form.get('name')
+        email = request.form.get('email')
+        subject = request.form.get('subject')
+        message = request.form.get('message')
+
+        msg = Message(
+            subject=f'Mail from {subject}',
+            sender=email,
+            recipients=[mail_username],
+            body= f'Name:{name}\nEmail:{email}\nMessage:{message}' 
+
+        )
+        mail.send(msg)
+
+        return render_template('contact.html', success = True)
+
+    return render_template('contact.html')
+
+@app.route("/manga")
+def manga():
+    return render_template('mangaList.html', data = getallManga())
+
+
+@app.route("/<string:url>")
+def mangaPage(url):
+    dict_mangaPage ={}
+    for key, value in getallManga():
+        if url == value['url']:
+            dict_mangaPage.update({key:value})
+            break
+     
+    return render_template('mangaPage.html', data = dict_mangaPage.items()) 
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
---
+++ model.py
@@ -0,0 +1,139 @@
+from ast import parse
+import json
+from pkgutil import iter_modules
+
+from sqlalchemy import values
+
+
+
+
+def parseJson():
+    with open('dbManga.json',encoding="utf8")  as f:
+        data = json.load(f)
+    return data
+   
+
+class Manga:
+    def __init__(self,nameManga,author,description,isUpdate,otherName,dateUpdate,category,chapter,cardImgUrl):
+        self.nameManga = nameManga
+        self.author = author
+        self.description = description
+        self.isUpdate = False
+        self.otherName = otherName
+        self.dateUpdate = dateUpdate
+        self.category = category
+        self.cardImgUrl = cardImgUrl
+        self.chapter = chapter['chap1']
+    
+
+mangaList = []
+def test():
+    with open('dbManga.json',encoding="utf8")  as f:
+        data = json.loads(f.read())
+        for item in data:
+            mangaList.append(Manga(**item))
+
+
+#index = 0
+#for index in range(len(parseJson())):
+#    for key, value in parseJson()[index].items():
+#        print(key," : ",value)
+#    print()
+
+
+
+def getallManga():
+    index_list = 0
+    dictMangaIndex ={}
+    for key , value in parseJson()[index_list].items():
+        dictMangaIndex.update({key:value})
+    return dictMangaIndex
+
+
+
+def mangaPage(id=3):
+    dict_mangaPage ={}
+    for key, value in getallManga():
+        if id == value['id']:
+            dict_mangaPage.update({key:value})
+            break
+    return dict_mangaPage.items()
+
+
+
+
+def mangaPageIndex(url):
+    temp= 0
+    dictMangaIndex ={}
+    for key,value in getallManga().items():
+        if url == value['url']:
+            dictMangaIndex.update({key:value})
+            temp = temp +1
+            if temp == 4:
+                break
+    return dictMangaIndex
+
+
+
+def parseChapter():
+    data = getallManga()
+    dict_chapter={}
+    value_data = {}
+    for key,value in data:
+        dict_chapter.update({key : value['chapter']})
+        value_data = values['id']
+    print(value_data)
+    return dict_chapter
+
+def mangaPageImg(nameManga,chap):
+    dictImg ={}
+    for key, value in getallManga().items():
+        if nameManga == value['nameManga']:
+            dictImg.update({key:value['chapter'][chap]})
+            break
+    return dictImg
+
+
+def chapter():
+    url = "kawaii-kanojo-chan"
+    url_chapter = []       
+    list_chapter =[]
+    for key,value in getallManga().items():
+        for item in value['chapter'].keys():
+            if url == value['url']:
+                list_chapter.append({key:item})
+    return list_chapter
+
+
+
+
+
+
+
+#nameManga = 'Kawaii Kanojo-chan'
+#chap = "chap-1"
+#list_chap = []
+#for item in mangaPageImg(nameManga,chap).values():
+#    for chap in item["imgUrl"].values():
+#        print(chap)
+
+#url ='kawaii-kanojo-chan'
+#print(mangaPageIndex(url))
+    
+
+def dictMangaIndex2():
+    dictMangaIndex2 ={}
+    temp= 0
+    for key,value in getallManga().items():
+        dictMangaIndex2.update({key:value})
+        temp = temp +1
+        if temp == 7:
+            break
+    print(reversed(dictMangaIndex2))
+
+dictMangaIndex2()
+
+
+
+
+
