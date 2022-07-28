cd45cc6dbcbeda9d50530d7a9dd27a9c6fc79446
VuNgoDinh <ngodinhvuluan@gmail.com>
2022-07-28 10:50:29 +0700

    a

--- parseJsonMangaPage.py
+++ parseJsonMangaPage.py
@@ -1,36 +1,28 @@
 import json
 
 def parseJsonMangadex():
-    with open('database.json',encoding="utf8") as f:
+    with open('dbManga.json',encoding="utf8") as f:
         data = json.loads(f.read())
     return data
 
 def idMangaJSON():
     idMangaList = {}
     for key,value in parseJsonMangadex().items():
-        idMangaList.update({key:value})
+        idMangaList.update({key:value['id']})
     return idMangaList
 
-
-
-def parseJsonIMG():
-    with open('dbManga.json',encoding="utf8") as f:
-        data = json.loads(f.read())
-    return data
 
 def imgMangaJSON():
     imgMangaList = {}
 
-    for keyID, valueID in idMangaJSON().items():
-        for keyImg,valueIMG in parseJsonIMG().items():
-            if keyID == keyImg:
-                 imgMangaList.update(
-                    {
-                        keyID:{
-                            "imgIndex":valueIMG['cardImgUrlIndex'],
-                            "imgBanner":valueIMG['cardImgUrl'],
-                            "imgCover":valueIMG['cardSingleImgUrl'],
-                        }
-                    }
-                )
+    for keyID, valueID in parseJsonMangadex().items():
+        imgMangaList.update(
+        {
+            keyID:{
+                "imgIndex":valueID['cardImgUrlIndex'],
+                "imgBanner":valueID['cardImgUrl'],
+                "imgCover":valueID['cardSingleImgUrl'],
+            }
+        }
+        )
     return imgMangaList
