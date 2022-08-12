dc400bc25490ac12ddbdb3578b65c7e88ebda115
VuNgoDinh <ngodinhvuluan@gmail.com>
2022-08-12 09:54:33 +0700

    a

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-from dbFireBase import getManga
-
-
-for key,value in getManga().items():
-    for chapter,valueChapter in value['chapter'].items():
-        print(key,chapter,valueChapter)
+import requests
