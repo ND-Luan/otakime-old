c6acaa6ebe77e97bdb15cfc25b274161ef4ebac6
VuNgoDinh <ngodinhvuluan@gmail.com>
2022-08-17 14:14:44 +0700

    a

--- pyrebaseLib/requests/__init__.py
+++ pyrebaseLib/requests/__init__.py
@@ -50,7 +50,7 @@ except ImportError:
     charset_normalizer_version = None
 
 try:
-    from chardet import __version__ as chardet_version
+    from pyrebaseLib.chardet import __version__ as chardet_version
 except ImportError:
     chardet_version = None
 
