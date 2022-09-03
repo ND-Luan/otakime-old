90e8868fbf8d4902e67436c19dacc881aae281e6
LamNhac <ngodinhluan567@gmail.com>
2022-09-03 11:14:33 +0700

    aa

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ app.config.update(dict(
 
 mail = Mail(app)
 
+
 def home():
     title= 'Otakime - Home'
     description = 'Trang web chính thức của nhóm dịch Otakime, Việt hóa những dự án manga nhằm giới thiệu độc giả. Truy cập ngay để đọc những tựa truyện được yêu thích.'
